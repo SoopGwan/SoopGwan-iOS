6954614135ef521a647a4940de41e598cbe9ce43
정대현 <eogus_2513@dsm.hs.kr>
2022-10-27 11:04:05 +0900

    feat: SendAuthCodeDTO 추가

---
+++ Service/Sources/DataMapping/Request/Users/SendAuthCodeDTO.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+public struct SendAuthCodeDTO: Codable {
+    let phoneNumber: String
+    
+    enum CondingKeys: String, CodingKey {
+        case phoneNumber = "phone_number"
+    }
+}
