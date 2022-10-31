4cb65cd6f7b7567f9be5e3d4a7d07e3b92f4c777
정대현 <eogus_2513@dsm.hs.kr>
2022-10-31 22:36:30 +0900

    feat: Setting Button 추가

---
+++ iOS/Sources/Components/Setting/SettingButton.swift
@@ -0,0 +1,37 @@
+import SwiftUI
+
+struct SettingButton<Destination>: View where Destination: View {
+    let title: String
+    let color: Color
+    let destination: () -> Destination
+
+    init(
+        title: String,
+        color: Color = .black,
+        destination: @escaping () -> Destination
+    ) {
+        self.title = title
+        self.color = color
+        self.destination = destination
+    }
+
+    var body: some View {
+        NavigationLink(destination: destination) {
+                HStack {
+                    Text(title)
+                        .font(.system(size: 18, weight: .semibold))
+                    Spacer()
+                    Image(systemName: "arrow.right")
+                        .resizable()
+                        .frame(width: 16, height: 14)
+                }
+                .padding(.horizontal, 18)
+                .foregroundColor(color)
+                .frame(height: 50)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.gray, lineWidth: 1)
+                }
+            }
+    }
+}
