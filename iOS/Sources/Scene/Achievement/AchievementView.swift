18942113213eaf983d723e4828ae1f8bc526fe65
정대현 <eogus_2513@dsm.hs.kr>
2022-10-31 22:36:55 +0900

    feat: TabView 설정

---
+++ iOS/Sources/Scene/Achievement/AchievementView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct AchievementView: View {
+    var body: some View {
+        Text("업적 화면입니다.")
+    }
+}
+
+struct AchieveView_Previews: PreviewProvider {
+    static var previews: some View {
+        AchievementView()
+    }
+}
