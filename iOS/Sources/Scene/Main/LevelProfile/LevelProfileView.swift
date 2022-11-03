522fa86bb8af2a0944ab60db9743075f7a861e9f
Daehee Kim <0824dh@naver.com>
2022-11-03 10:54:30 +0900

    feat :: LevelProfileView shadow 추가

--- iOS/Sources/Scene/Main/LevelProfile/LevelProfileView.swift
+++ iOS/Sources/Scene/Main/LevelProfile/LevelProfileView.swift
@@ -2,29 +2,36 @@ import SwiftUI
 
 struct LevelProfileView: View {
     var body: some View {
-        HStack {
-            Spacer()
-            VStack(
-                alignment: .center,
-                spacing: 0
-            ) {
-                Text("")
-                    .font(.system(size: 20, weight: .bold))
-                    .foregroundColor(Color("3D8361"))
-                Image("")
-                    .resizable()
-                    .frame(width: 200, height: 200)
-                Text("")
-                    .font(.system(size: 12, weight: .bold))
-                    .foregroundColor(.gray)
+        ZStack {
+            Color.white
+            HStack {
+                Spacer()
+                VStack(
+                    alignment: .center,
+                    spacing: 0
+                ) {
+                    Text("LV 3")
+                        .font(.system(size: 20, weight: .bold))
+                        .foregroundColor(Color("3D8361"))
+                    Image("")
+                        .resizable()
+                        .frame(width: 200, height: 200)
+                    Text("")
+                        .font(.system(size: 12, weight: .bold))
+                        .foregroundColor(.gray)
+
+                }
+                Spacer()
             }
-            .frame(width: .infinity, height: 300)
-            Spacer()
         }
+        .frame(height: 300)
         .overlay {
             RoundedRectangle(cornerRadius: 32)
                 .stroke(Color("2F6149"), lineWidth: 2)
         }
+
+        .cornerRadius(32, antialiased: true)
+        .shadow(color: Color("797F53"), radius: 32, x: 0, y: 4)
     }
 }
 
