2fed30112314deea893408d44a46e4c73f4ebbb4
Daehee Kim <0824dh@naver.com>
2022-11-08 19:03:24 +0900

    feat :: RemoteUserDataSource & impl

---
+++ Service/Sources/Network/User/RemoteUserDataSourceImpl.swift
@@ -0,0 +1,30 @@
+//
+//  RemoteUserDataSourceImpl.swift
+import Combine
+
+public final class RemoteUserDataSourceImpl: BaseRemoteDataSource<UserAPI>, RemoteUserDataSource {
+    public func signin(req: SignInRequestDTO) -> AnyPublisher<Void, SoopGwanError> {
+        request(.signin(content: req))
+            .eraseToAnyPublisher()
+    }
+
+    public func signup(req: SignUpRequestDTO) -> AnyPublisher<Void, SoopGwanError> {
+        request(.signup(content: req))
+            .eraseToAnyPublisher()
+    }
+
+    public func verifyAuthCode(req: VerifyAuthCodeRequestDTO) -> AnyPublisher<Void, SoopGwanError> {
+        request(.verifyAuthCode(content: req))
+            .eraseToAnyPublisher()
+    }
+
+    public func checkAccountIDIsExist(accountID: String) -> AnyPublisher<Void, SoopGwanError> {
+        request(.checkAccountIDIsExist(accountID: accountID))
+            .eraseToAnyPublisher()
+    }
+
+    public func renewalPassword(req: RenewalPasswordRequestDTO) -> AnyPublisher<Void, SoopGwanError> {
+        request(.renewalPassword(content: req))
+            .eraseToAnyPublisher()
+    }
+}
