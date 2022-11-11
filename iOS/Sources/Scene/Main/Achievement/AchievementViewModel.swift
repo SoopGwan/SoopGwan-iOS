a43cc5928574b2bf55485c4e248779d4f75ad961
Daehee Kim <0824dh@naver.com>
2022-11-11 14:08:07 +0900

    feat: AchievementViewModel 더미 모델 생성

--- iOS/Sources/Scene/Main/Achievement/AchievementViewModel.swift
+++ iOS/Sources/Scene/Main/Achievement/AchievementViewModel.swift
@@ -1,5 +1,31 @@
 import Combine
+import Service
 
 final class AchievementViewModel: BaseViewModel {
-
+    let list: [AchievementsListEntity] = [
+        .init(
+            title: "1주 동안 한번도 습관 실천하지 않기",
+            date: "2022-01-01 달성",
+            rarityType: "color",
+            productType: "dislike_badge"
+        ),
+        .init(
+            title: "1일 동안 한번도 습관 실천하지 않기",
+            date: "2022-02-12일 달성",
+            rarityType: "normal",
+            productType: "dislike_badge"
+        ),
+        .init(
+            title: "1일 동안 한번도 습관 실천하지 않기",
+            date: "2022-02-12일 달성",
+            rarityType: "normal",
+            productType: "dislike_badge"
+        ),
+        .init(
+            title: "오전 12:00에 습관 실천하기",
+            date: "2022-05-05일 달성",
+            rarityType: "gradation",
+            productType: "alarm_clock"
+        ),
+    ]
 }
