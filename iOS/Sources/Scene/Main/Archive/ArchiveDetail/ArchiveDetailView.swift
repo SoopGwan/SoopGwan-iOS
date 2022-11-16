a06762c1f08e819c876534c5d47e5400e3a5be27
Daehee Kim <0824dh@naver.com>
2022-11-16 23:13:28 +0900

    feat: ArchiveDetailView 완성

--- iOS/Sources/Scene/Main/Archive/ArchiveDetail/ArchiveDetailView.swift
+++ iOS/Sources/Scene/Main/Archive/ArchiveDetail/ArchiveDetailView.swift
@@ -3,6 +3,7 @@ import SwiftUI
 struct ArchiveDetailView: View {
     @State private var showModal = false
     @StateObject var viewModel = ArchiveDetailViewModel()
+    @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
 
     private let id: Int
     private let level: Int
@@ -26,10 +27,14 @@ struct ArchiveDetailView: View {
             Text(level.levelToContent())
                 .font(.system(size: 18, weight: .medium))
                 .foregroundColor(Color("555555"))
+                .padding(.horizontal, 30)
+                .padding(.bottom, 1)
 
             Text(date)
-                .font(.system(size: 15, weight: .medium))
+                .font(.system(size: 14, weight: .medium))
                 .foregroundColor(Color("999999"))
+                .padding(.horizontal, 30)
+
             Spacer()
                 .frame(height: 20)
 
@@ -40,14 +45,15 @@ struct ArchiveDetailView: View {
 
                 Spacer()
 
-                VStack {
+                VStack(alignment: .trailing) {
                     Text(level.levelToTitle())
-                        .font(.system(size: 12, weight: .medium))
+                        .font(.system(size: 14, weight: .medium))
                         .foregroundColor(Color("4A4A4A"))
                         .padding(.bottom, 8)
 
                     Text(viewModel.detailMessage)
-                        .font(.system(size: 8, weight: .medium))
+                        .multilineTextAlignment(.trailing)
+                        .font(.system(size: 10, weight: .medium))
                         .foregroundColor(Color("999999"))
                 }
                 .padding(.trailing, 22)
@@ -56,10 +62,73 @@ struct ArchiveDetailView: View {
                 RoundedRectangle(cornerRadius: 10)
                     .stroke(Color("2F6149"), lineWidth: 1)
             }
+            .padding(.horizontal, 30)
 
             Spacer()
+                .frame(height: 20)
+
+            HStack {
+                VStack(alignment: .leading) {
+                    Spacer()
+
+                    Text("습관 실천 평가 결과")
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(Color("4A4A4A"))
+
+                    Spacer()
+                        .frame(height: 5)
+
+                    Text("기분이 좋아요!")
+                        .font(.system(size: 10, weight: .medium))
+                        .foregroundColor(Color("999999"))
+
+                    Spacer()
+                }
+                .padding(.leading, 5)
+
+                Spacer()
+
+                Image("Emoji-1")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+            }
+            .padding(.horizontal, 15)
+            .frame(height: 70)
+            .overlay {
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color("2F6149"), lineWidth: 1)
+            }
+            .padding(.horizontal, 30)
+
+            Spacer()
+
+            ScrollView(
+                .vertical,
+                showsIndicators: false
+            ) {
+                ForEach(viewModel.list, id: \.self) { list in
+                    ArchiveDetailCellView(
+                        title: list.title,
+                        content: list.content
+                    )
+                    .padding(.horizontal, 30)
+
+                }
+            }
+
+            Button {
+                self.presentationMode.wrappedValue.dismiss()
+            } label: {
+                Text("아카이브로 돌아가기")
+                    .font(.system(size: 15, weight: .medium))
+                    .foregroundColor(.white)
+                    .frame(maxWidth: .infinity, maxHeight: 48)
+                    .background(Color("66A865"))
+                    .cornerRadius(5)
+            }
+            .padding(.horizontal, 24)
+
         }
-        .padding(.horizontal, 30)
         .onAppear {
             viewModel.onAppear(id: id, level: level, date: date)
         }
