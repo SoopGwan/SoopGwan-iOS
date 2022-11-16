109bc3d1f5dfbe385bae1e6648d16eccaaf17443
Daehee Kim <0824dh@naver.com>
2022-11-16 11:05:36 +0900

    feat: DropDown DropdownOptionElement 감싸는 뷰 추가

--- iOS/Sources/Components/Dropdown/Dropdown.swift
+++ iOS/Sources/Components/Dropdown/Dropdown.swift
@@ -6,4 +6,33 @@
 //  Copyright © 2022 com.SoopGwan. All rights reserved.
 //
 
-import Foundation
+import SwiftUI
+
+struct Dropdown: View {
+    private let dropdownCornerRadius: CGFloat = 8.0
+
+    var options: [DropdownOption]
+    var onSelect: ((_ key: String) -> Void)?
+
+    var body: some View {
+        ZStack {
+            Color.white
+            VStack(alignment: .leading, spacing: 0) {
+                ForEach(self.options, id: \.self) { option in
+                    DropdownOptionElement(
+                        val: option.val,
+                        key: option.key,
+                        onSelect: self.onSelect
+                    )
+                }
+        }
+        }
+
+        .background(Color.white)
+        .cornerRadius(dropdownCornerRadius)
+        .overlay(
+            RoundedRectangle(cornerRadius: dropdownCornerRadius)
+                .stroke(Color("3D8361"), lineWidth: 1)
+        )
+    }
+}
