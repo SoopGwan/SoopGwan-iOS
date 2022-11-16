6832db837203f293cf24b4bf005f99356cb9db3f
Daehee Kim <0824dh@naver.com>
2022-11-16 11:05:55 +0900

    feat: DropdownOptionElement 세부사항 뷰 추가

--- iOS/Sources/Components/Dropdown/DropdownOptionElement.swift
+++ iOS/Sources/Components/Dropdown/DropdownOptionElement.swift
@@ -1,9 +1,27 @@
-//
-//  DropdownOptionElement.swift
-//  SoopGwan
-//
-//  Created by 김대희 on 2022/11/15.
-//  Copyright © 2022 com.SoopGwan. All rights reserved.
-//
+import SwiftUI
 
-import Foundation
+struct DropdownOptionElement: View {
+    private let dropdownCornerRadius: CGFloat = 8.0
+
+    var val: String
+    var key: String
+    var onSelect: ((_ key: String) -> Void)?
+
+    var body: some View {
+        ZStack {
+            Color.white
+
+            Button {
+                if let onSelect = self.onSelect {
+                    onSelect(self.key)
+                }
+            } label: {
+                Text(self.val)
+                    .font(.system(size: 16, weight: .medium))
+                    .foregroundColor(Color("3D8361"))
+            }
+        }
+    .padding(.horizontal, 20)
+    .padding(.vertical, 5)
+    }
+}
