b8c830d926a305d520fb51c3aad7d20cdfb99607
Daehee Kim <0824dh@naver.com>
2022-11-16 18:51:21 +0900

    feat:  CheckBok

--- iOS/Sources/Scene/Main/Home/LevelProfileView.swift
+++ iOS/Sources/Scene/Main/Home/LevelProfileView.swift
@@ -6,27 +6,29 @@ struct LevelProfileView: View {
         ZStack {
             Color.white
             VStack(alignment: .center) {
+                Spacer()
+                    .frame(height: 5)
                 Text(level.levelToTitle())
                     .font(.system(size: 20, weight: .bold))
                     .foregroundColor(Color("3D8361"))
 
                 Image(level.levelToImageName())
                     .resizable()
-                    .frame(width: 200, height: 200)
+                    .frame(width: 210, height: 210)
                     .padding(.vertical, 8)
 
                 Text(level.levelToContent())
                     .font(.system(size: 13, weight: .bold))
                     .foregroundColor(.gray)
             }
+            .padding(.vertical, 20)
+
         }
-        .frame(height: 320)
         .overlay {
             RoundedRectangle(cornerRadius: 32)
-                .stroke(Color("2F6149"), lineWidth: 2)
+                .stroke(Color("2F6149"), lineWidth: 4)
         }
         .cornerRadius(32, antialiased: true)
-        .shadow(color: Color("797F53"), radius: 32, x: 0, y: 4)
 
     }
 }
