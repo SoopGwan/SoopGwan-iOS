9128893579b92a423a93b4037ce263cfb370a37b
Daehee Kim <0824dh@naver.com>
2022-11-16 21:02:59 +0900

    feat: ArchiveDetailViewModel detailMessage

--- iOS/Sources/Scene/Main/Archive/ArchiveDetail/ArchiveDetailViewModel.swift
+++ iOS/Sources/Scene/Main/Archive/ArchiveDetail/ArchiveDetailViewModel.swift
@@ -6,7 +6,13 @@ final class ArchiveDetailViewModel: BaseViewModel {
     private let level: Int = 0
     private let date: String = ""
 
+    @Published var detailMessage = """
+        일주일 동안 빠짐없이 실천한 습관 2개
+        습관 5개 한번 이상 실천
+        습관 1개 한번도 실천하지 않음
+        화이팅입니다.
+        """
     func onAppear(id: Int, level: Int, date: String) {
-        
+
     }
 }
