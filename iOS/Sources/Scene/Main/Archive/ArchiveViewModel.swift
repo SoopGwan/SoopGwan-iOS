853442831efcf55fc29d43525eea60380a616c0d
Daehee Kim <0824dh@naver.com>
2022-11-16 11:03:10 +0900

    feat: ArchiveViewModel dummy

--- iOS/Sources/Scene/Main/Archive/ArchiveViewModel.swift
+++ iOS/Sources/Scene/Main/Archive/ArchiveViewModel.swift
@@ -1,5 +1,99 @@
 import Combine
+import Service
 
 final class ArchiveViewModel: BaseViewModel {
+    @Published var yearSelect: String = "2022년"
+    @Published var monthSelect: String = "11월"
 
+    @Published var list: [ArchiveHabitEntity] = [
+        .init(
+            id: 1,
+            startAt: "2022-01-01",
+            endAt: "2022-01-06",
+            level: 0
+        ),
+        .init(
+            id: 1,
+            startAt: "2022-01-01",
+            endAt: "2022-01-06",
+            level: 1
+        ),
+        .init(
+            id: 1,
+            startAt: "2022-01-01",
+            endAt: "2022-01-06",
+            level: 2
+        ),
+        .init(
+            id: 1,
+            startAt: "2022-01-01",
+            endAt: "2022-01-06",
+            level: 3
+        ),
+        .init(
+            id: 1,
+            startAt: "2022-01-01",
+            endAt: "2022-01-06",
+            level: 4
+        )
+    ]
+
+    @Published var yearOptions = [
+        DropdownOption(
+            key: "2022년",
+            val: "2022년"),
+        DropdownOption(
+            key: "2021년",
+            val: "2021년"
+        ),
+        DropdownOption(
+            key: "2020년",
+            val: "2020년"
+        )
+    ]
+    @Published var monthOptions = [
+        DropdownOption(
+            key: "11월",
+            val: "11월"),
+        DropdownOption(
+            key: "10월",
+            val: "10월"
+        ),
+        DropdownOption(
+            key: "9월",
+            val: "9월"
+        ),
+        DropdownOption(
+            key: "8월",
+            val: "8월"
+        ),
+        DropdownOption(
+            key: "7월",
+            val: "7월"
+        ),
+        DropdownOption(
+            key: "6월",
+            val: "6월"
+        ),
+        DropdownOption(
+            key: "5월",
+            val: "5월"
+        ),
+        DropdownOption(
+            key: "4월",
+            val: "4월"
+        ),
+        DropdownOption(
+            key: "3월",
+            val: "3월"
+        ),
+        DropdownOption(
+            key: "2월",
+            val: "2월"
+        ),
+        DropdownOption(
+            key: "1월",
+            val: "1월"
+        )
+    ]
 }
