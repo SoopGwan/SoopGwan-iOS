980b04d8e687c5a70ff945f6df4de79e3b672f0c
Daehee Kim <0824dh@naver.com>
2022-11-16 18:52:21 +0900

    feat: WeekHabitEntity public 변경 후 더미 작성

--- Service/Sources/Entity/WeekHabitEntity.swift
+++ Service/Sources/Entity/WeekHabitEntity.swift
@@ -1,7 +1,17 @@
 import Foundation
 
-public struct WeekHabitEntity: Equatable {
-    let id: Int
-    let title: String
-    let successStatus: Bool
+public struct WeekHabitEntity: Equatable, Hashable {
+    public let id: Int
+    public let title: String
+    public let successStatus: Bool
+
+    public init(
+        id: Int,
+        title: String,
+        successStatus: Bool
+    ) {
+        self.id = id
+        self.title = title
+        self.successStatus = successStatus
+    }
 }
--- iOS/Sources/Scene/Main/Home/HomeViewModel.swift
+++ iOS/Sources/Scene/Main/Home/HomeViewModel.swift
@@ -1,5 +1,32 @@
 import Combine
+import Service
 
 final class HomeViewModel: BaseViewModel {
-
+    @Published var habitList: [WeekHabitEntity] = [
+        .init(id: 0, title: "리치 밥주기", successStatus: true),
+        .init(id: 0, title: "아보카도 물주기", successStatus: false),
+        .init(id: 0, title: "정해진 시간에 일어나기", successStatus: true),
+        .init(id: 0, title: "일어나서 물 마시기", successStatus: false),
+        .init(id: 0, title: "리치 밥주기", successStatus: true),
+        .init(id: 0, title: "아보카도 물주기", successStatus: false),
+        .init(id: 0, title: "정해진 시간에 일어나기", successStatus: true),
+        .init(id: 0, title: "일어나서 물 마시기", successStatus: false),
+        .init(id: 0, title: "리치 밥주기", successStatus: true),
+        .init(id: 0, title: "아보카도 물주기", successStatus: false),
+        .init(id: 0, title: "정해진 시간에 일어나기", successStatus: true),
+        .init(id: 0, title: "일어나서 물 마시기", successStatus: false),
+        .init(id: 0, title: "리치 밥주기", successStatus: true),
+        .init(id: 0, title: "아보카도 물주기", successStatus: false),
+        .init(id: 0, title: "정해진 시간에 일어나기", successStatus: true),
+        .init(id: 0, title: "일어나서 물 마시기", successStatus: false),
+        .init(id: 0, title: "리치 밥주기", successStatus: true),
+        .init(id: 0, title: "아보카도 물주기", successStatus: false),
+        .init(id: 0, title: "정해진 시간에 일어나기", successStatus: true),
+        .init(id: 0, title: "일어나서 물 마시기", successStatus: false),
+        .init(id: 0, title: "리치 밥주기", successStatus: true),
+        .init(id: 0, title: "아보카도 물주기", successStatus: false),
+        .init(id: 0, title: "정해진 시간에 일어나기", successStatus: true),
+        .init(id: 0, title: "일어나서 물 마시기", successStatus: false),
+        .init(id: 0, title: "잠자기 전 책 10페이지 읽기", successStatus: false)
+    ]
 }
