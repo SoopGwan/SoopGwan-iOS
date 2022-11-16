55d7a56b3c617139ac0b172a9108832cc76125ff
Daehee Kim <0824dh@naver.com>
2022-11-16 10:49:08 +0900

    fix: ArchiveHabitEntity Hashable 추가, public으로 변경

--- Service/Sources/Entity/ArchiveHabitEntity.swift
+++ Service/Sources/Entity/ArchiveHabitEntity.swift
@@ -1,7 +1,19 @@
 import Foundation
 
-public struct ArchiveHabitEntity: Equatable {
-    let id: Int
-    let startAt, endAt: String
-    let level: Int
+public struct ArchiveHabitEntity: Hashable, Equatable {
+    public init(
+        id: Int,
+        startAt: String,
+        endAt: String,
+        level: Int
+    ) {
+        self.id = id
+        self.startAt = startAt
+        self.endAt = endAt
+        self.level = level
+    }
+
+    public let id: Int
+    public let startAt, endAt: String
+    public let level: Int
 }
