611c71dbe91a1ddacd4e3eb79f1b247b6f19502a
Daehee Kim <0824dh@naver.com>
2022-11-16 19:35:19 +0900

    feat: ArchiveDetailViewModel 기본 설정

---
+++ iOS/Sources/Scene/Main/Archive/ArchiveDetail/ArchiveDetailViewModel.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+import Combine
+
+final class ArchiveDetailViewModel: BaseViewModel {
+    private let id: Int = 0
+    private let level: Int = 0
+    private let date: String = ""
+
+    func onAppear(id: Int, level: Int, date: String) {
+        
+    }
+}
