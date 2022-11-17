e2b7413cd8c73444168ead8de0d5694b67601a24
Daehee Kim <0824dh@naver.com>
2022-11-17 18:19:47 +0900

    feat: SoopGwan 토스트 맹들기

---
+++ iOS/Sources/Components/Alert/SoopGwanToast.swift
@@ -0,0 +1,88 @@
+import SwiftUI
+
+struct SoopGwanToast: ViewModifier {
+    @Binding var isShowing: Bool
+    let message: String
+    let style: DmsToastStyle
+
+    init(
+        isShowing: Binding<Bool>,
+        message: String,
+        style: DmsToastStyle
+    ) {
+        _isShowing = isShowing
+        self.message = message
+        self.style = style
+    }
+
+    func body(content: Content) -> some View {
+        ZStack {
+            content
+
+            dmsToastView()
+        }
+        .onChange(of: isShowing) { _ in
+            if isShowing {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        isShowing = false
+                    }
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func dmsToastView() -> some View {
+        VStack {
+            if isShowing {
+                HStack(spacing: 10) {
+                    style.icon
+                        .resizable()
+                        .renderingMode(.template)
+                        .frame(width: style.size.width, height: style.size.height)
+                        .foregroundColor(style.iconForeground)
+
+                    Text(message)
+                        .font(.system(size: 14, weight: .medium))
+                        .foregroundColor(style.textForeground)
+
+                    Spacer()
+                }
+                .transition(.offset(y: -50).combined(with: AnyTransition.opacity.animation(.default)))
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 14)
+                .padding(.horizontal, 16)
+                .background {
+                    Rectangle()
+                        .fill(.white)
+                        .cornerRadius(4)
+                        .shadow(
+                            color: .gray.opacity(0.08),
+                            radius: 4,
+                            y: 1
+                        )
+                }
+                .padding(.horizontal, 12)
+                .onTapGesture {
+                    withAnimation {
+                        isShowing = false
+                    }
+                }
+            }
+
+            Spacer()
+        }
+        .animation(.default, value: isShowing)
+    }
+}
+
+public extension View {
+    func dmsToast(
+        isShowing: Binding<Bool>,
+        message: String,
+        style: DmsToastStyle
+    ) -> some View {
+        modifier(SoopGwanToast(isShowing: isShowing, message: message, style: style))
+    }
+}
