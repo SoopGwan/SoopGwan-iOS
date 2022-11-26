205a9dfd0d6c964cb42d055f4d226a55c37179fe
Daehee Kim <0824dh@naver.com>
2022-11-26 21:04:27 +0900

    feat: AchieveListEntity

---
+++ Service/Sources/Entity/AchieveListEntity.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+public struct AchieveListEntity: Equatable {
+    public let title: String
+    public let imageName: String
+    public let content: String
+
+    public init(
+        title: String,
+        imageName: String,
+        content: String
+    ) {
+        self.title = title
+        self.imageName = imageName
+        self.content = content
+    }
+}
