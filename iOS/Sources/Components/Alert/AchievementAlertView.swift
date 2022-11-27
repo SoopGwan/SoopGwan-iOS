14a3a706f64c6aa030d304598d5530797cac544c
Daehee Kim <0824dh@naver.com>
2022-11-27 17:33:42 +0900

    feat: AchievementAlertView 데이터 바인딩

--- iOS/Sources/Components/Alert/AchievementAlertView.swift
+++ iOS/Sources/Components/Alert/AchievementAlertView.swift
@@ -22,35 +22,38 @@ struct AchievementAlertView: View {
         ZStack {
             Color.black.opacity(0.44)
                 .ignoresSafeArea()
+
             VStack(alignment: .center) {
-                Text("하루에 하나 이상 1달간 습관 실천하기")
+                Text(title)
                     .font(.system(size: 16, weight: .semibold))
                     .padding(.top, 40)
-                Text("2022-08-24 달성")
+
+                Text("\(imageName)를 획득했어요!")
                     .foregroundColor(Color(uiColor: .lightGray))
                     .font(.system(size: 12, weight: .medium))
                     .padding(.top, 8)
-                Image("premium-vault")
+
+                Image(imageName)
                     .resizable()
                     .frame(width: 200, height: 200)
-                Text("""
-                     앗! Premium 금고를 획득했어요!
-                     1달간 꾸준히 습관을 실천한다는 것은 정말 대단한 것이에요.
-                     자 그럼 이제 금고를 가져가세요.
-                     아! 말 안한게 있었네요.  비밀번호는 몰라요.
-                """)
+
+                Text(content)
                 .font(.system(size: 12, weight: .bold))
                 .padding(.top, 20)
+
                 Button(action: action) {
                     Text("닫기")
                         .font(.system(size: 12, weight: .medium))
                 }
                 .foregroundColor(Color("2F6149"))
                 .padding(.vertical, 40)
+
             }
-            .padding(.horizontal, 39)
+            .padding(.horizontal, 30)
             .background(Color.white)
             .cornerRadius(32)
+            .padding(.horizontal, 39)
+
         }
     }
 }
